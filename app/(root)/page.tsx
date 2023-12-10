145eb44f9315e0435efe3babfb4e27e1d104443c
Dhaval Panchal <panchaldhaval049@gmail.com>
2023-12-10 12:25:48 +0530

    Added shadcn-ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center gap-1 flex-col items-center h-screen">
+      <p>Hello world</p>
+      <Button size="sm">Click me</Button>
+    </div>
+  )
+}
